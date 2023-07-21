69189f07b101be18852b4cd4e38fc4a40b359d9c
Fang Chun <fangchun@starsplash.com.my>
2023-07-21 11:40:25 +0800

    Implemented feature to fetch data from google spreadsheet to unity.

--- Assets/Scripts/_Testing/DatabaseViewer.cs
+++ Assets/Scripts/_Testing/DatabaseViewer.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DatabaseViewer : MonoBehaviour
 {
-    private DatabaseManager.Character character;
-    private DatabaseManager.Skill skill;
-    private DatabaseManager.Subskill subskill;
+    private List<DatabaseManager.Character> characterList = new List<DatabaseManager.Character>();
+    private List<DatabaseManager.Skill> skillList = new List<DatabaseManager.Skill>();
+    private List<DatabaseManager.Subskill> subskillList = new List<DatabaseManager.Subskill>();
+
+    void Start()
+    {
+        DatabaseManager.Instance.onDataUpdatedCallback = DisplayAllData;
+    }
+
+    private void DisplayAllData()
+    {
+        this.characterList = DatabaseManager.Instance.GetCharacterList();
+        this.skillList = DatabaseManager.Instance.GetSkillList();
+        this.subskillList = DatabaseManager.Instance.GetSubskillList();
+    }
 }
