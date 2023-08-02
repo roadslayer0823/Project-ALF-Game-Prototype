5dc178495d5d7b438873f1290a713b1ef24317df
Fang Chun <fangchun@starsplash.com.my>
2023-08-02 16:39:40 +0800

    - Added "Versions" and "Skill_Animations" inner class. - Implemented code for getting the "Versions" and "Skill_Animations" data from Google Spreadsheet. - Added new save and load functions in PlayerPrefsManager for "Versions" and "Skill_Animations". - Do some corrections. - *Currently still half way implementing the check version function, will continue doing it after this commit.*

--- Assets/Scripts/Managers/PlayerPrefsManager.cs
+++ Assets/Scripts/Managers/PlayerPrefsManager.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 public class PlayerPrefsManager
 {
+    private const string VERSION_DATABASE = "version_database";
     private const string CHARACTER_DATABASE = "character_database";
     private const string SKILL_DATABASE = "skill_database";
     private const string SUBSKILL_DATABASE = "subskill_database";
+    private const string SKILL_ANIMATION_DATABASE = "skill_animation_database";
+
+    public static void SaveVersionDatabase(string jsonData)
+    {
+        PlayerPrefs.SetString(VERSION_DATABASE, jsonData);
+    }
+
+    public static string LoadVersionDatabase()
+    {
+        return PlayerPrefs.GetString(VERSION_DATABASE, "");
+    }
 
     public static void SaveCharacterDatabase( string jsonData )
     {
@@ -34,5 +46,15 @@ public class PlayerPrefsManager
     public static string LoadSubskillDatabase()
     {
         return PlayerPrefs.GetString( SUBSKILL_DATABASE, "" );
+    }
+
+    public static void SaveSkillAnimationDatabase(string jsonData)
+    {
+        PlayerPrefs.SetString(SKILL_ANIMATION_DATABASE, jsonData);
+    }
+
+    public static string LoadSkillAnimationDatabase()
+    {
+        return PlayerPrefs.GetString(SKILL_ANIMATION_DATABASE, "");
     }
 }
