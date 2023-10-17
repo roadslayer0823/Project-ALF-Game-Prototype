a005eda76041137d5da00574c1c9a113245a016a
LeeKahHin <kahhin@starsplash.com.my>
2023-10-17 11:59:31 +0800

    adding the terminology manager and declaring the variable name(use to reference on the display text)

---
+++ Assets/Scripts/Managers/TerminologyManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TerminologyManager : MonoBehaviour
+{
+    public const string STATE_POINT = "以太";
+    public const string BREAK = "崩潰";
+    public const string KNOCKOUT = "擊飛";
+    public const string STRESS_VALUE = "負荷";
+
+}
