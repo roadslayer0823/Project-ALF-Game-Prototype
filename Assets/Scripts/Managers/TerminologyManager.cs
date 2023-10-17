f1ff9711bcff32dc7681ea4bece374a48e6cc9f6
LeeKahHin <kahhin@starsplash.com.my>
2023-10-17 13:52:56 +0800

    changing the stress_value into stress_point.

--- Assets/Scripts/Managers/TerminologyManager.cs
+++ Assets/Scripts/Managers/TerminologyManager.cs
@@ -5,5 +5,5 @@ public class TerminologyManager
     public const string STATE_POINT = "以太值";
     public const string BREAK = "崩潰";
     public const string KNOCKOUT = "擊飛";
-    public const string STRESS_VALUE = "負荷值";
+    public const string STRESS_POINT = "負荷值";
 }
