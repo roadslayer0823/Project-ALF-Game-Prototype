074cec165a22b7452f01fb680a99513a53b8abf4
LeeKahHin <kahhin@starsplash.com.my>
2023-11-02 11:16:16 +0800

    Passive Skill Selection Panel: 	-adding the UI 	-adding two type open the UI which is hold and click: 		-hold: when the player hold the button the related passive skill will be show up, when the release it will close it. When the player 			  wanna to choose the passive skill need to drag to the related option by holding the button. 		-click: when the player click on the character info panel, it will show up the UI and once the player click the character info panel again 			   it will close it. when the player wanna to choose the passive skill they need to click on the related passive skill after opened the 			   UI 	-when the player is choose the related passive skill the text and color of the button will be change to the passive skill choose by player, to 	 easily know what passive skill is been choosen

---
+++ Assets/Scripts/Scenes/BattleScene/UserInterfaces/PassiveSkillCategorySelectionPanel/PassiveSkillCategorySelectionPanel.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class PassiveSkillCategorySelectionPanel : MonoBehaviour
+{ 
+    [Header("Reference")]
+    [SerializeField] private GameObject passiveSkillHolder;
+    [SerializeField] private TextMeshProUGUI passiveSkillName;
+    [SerializeField] private Image passiveSkillButton;
+    [SerializeField] private Image statePointSkillColor;
+    [SerializeField] private Image stressPointSkillColor;
+    [SerializeField] private Image healthPointSkillColor;
+
+    private TextMeshProUGUI selectedOption = null;
+    private bool isSelecting = false;
+    private bool isHolding = false;
+    private bool isClicked = false;
+
+    public void HoldButton()
+    {
+        isHolding = true;
+        passiveSkillHolder.SetActive(true);
+    }
+
+    public void ReleaseHoldButton()
+    {
+        isHolding = false;
+        isSelecting = false;
+        selectedOption = null;
+        passiveSkillHolder.SetActive(false);
+    }
+
+    public void ClickButton()
+    {
+        if(isClicked == false)
+        {
+            isClicked = true;
+            passiveSkillHolder.SetActive(true);
+        }
+        else if(isClicked == true)
+        {
+            isClicked = false;
+            isSelecting = false;
+            passiveSkillHolder.SetActive(false);
+        }
+    }
+
+    public void ClickOption(TextMeshProUGUI option)
+    {
+        if(isClicked == true)
+        {
+            isSelecting = true;
+            selectedOption = option;
+            passiveSkillName.text = selectedOption.text;
+            changeButtonColor();
+            Debug.Log(selectedOption.name);
+        }
+    }
+
+    public void OptionEnter(TextMeshProUGUI option)
+    {
+        if (isHolding == true && isClicked == false)
+        {
+            isSelecting = true;
+            selectedOption = option;
+            passiveSkillName.text = selectedOption.text;
+            changeButtonColor();
+        }
+    }
+
+    public void changeButtonColor()
+    {
+        if(passiveSkillName.text == "以太流")
+        {
+            passiveSkillButton.color = statePointSkillColor.color;
+        }
+        else if(passiveSkillName.text == "生命流")
+        {
+            passiveSkillButton.color = healthPointSkillColor.color;
+        }
+        else if(passiveSkillName.text == "负荷流")
+        {
+            passiveSkillButton.color = stressPointSkillColor.color;
+        }
+    }
+
+    public void onOptionExit()
+    {
+        if (isSelecting == true && selectedOption != null && isHolding == true)
+        {
+            Debug.Log("選擇:" + selectedOption.text);
+        }
+    }
+}
