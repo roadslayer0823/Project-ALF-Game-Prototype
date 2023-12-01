55186cb3c30ee407f8c4a369903305a0fd49b505
Fang Chun <84074336+Chun-888@users.noreply.github.com>
2023-12-01 12:29:38 +0800

    - Change skill prompt panel from scene to UI. - Make skill prompt panel v1 and v2 to prefab. - Adjust the related asset files for skill prompt panel folder. - Recreate a new animation clips and animation controllers for skill prompt panel v2. - Recreate a new script for skill prompt panel v2. - Remove the event key in skill prompt panel v1 animation clip. - Make the skill name able to prompt even when used regardless speed and strength. - Import active skill and backend skill background image assets for the skill name box. - Do some corrections.

---
+++ Assets/Scripts/Scenes/BattleScene/UserInterfaces/SkillPromptPanel/SkillPromptPanelV2.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillPromptPanelV2 : MonoBehaviour
+{
+    [SerializeField] private Animator speedEffectAnimator = null;
+    [SerializeField] private Animator strengthEffectAnimator = null;
+
+    [SerializeField] private GameObject skillNameGO = null;
+    [SerializeField] private TextMeshProUGUI skillNameText = null;
+    [SerializeField] private GameObject enemyPassiveSkillName = null;
+    [SerializeField] private TextMeshProUGUI enemyPassiveSkillNameText = null;
+    [SerializeField] private GameObject playerPassiveSkillName = null;
+    [SerializeField] private TextMeshProUGUI playerPassiveSkillNameText = null;
+    [SerializeField] private GameObject speedEffectGO = null;
+    [SerializeField] private GameObject strengthEffectGO = null;
+    [SerializeField] private Image skillNameBackgroundImage = null;
+
+    [Header("SkillNameBackground")]
+    [SerializeField] private Sprite activeSkillBackgroundImage = null;
+    [SerializeField] private Sprite backendSkillBackgroundImage = null;
+
+    [Header("Setting")]
+    [SerializeField] private float preferredPercentage = 0.8f;
+
+    public void Show(GameCharacter caster, float partADuration)
+    {
+        CharacterSkill _characterSkill = caster.GetCurrentSkill();
+        int _skillStatIncrement = caster.GetCurrentSkillStatIncrement();
+
+        if (_characterSkill == null || this.skillNameGO.activeInHierarchy)
+        {
+            return;
+        }
+
+        this.speedEffectGO.gameObject.SetActive(true);
+        this.strengthEffectGO.gameObject.SetActive(true);
+
+        if (_characterSkill.GetSkillData().skillType == DatabaseManager.Skill.SkillType.active)
+        {
+            this.skillNameBackgroundImage.sprite = this.activeSkillBackgroundImage;
+        }
+        else if (_characterSkill.GetSkillData().skillType == DatabaseManager.Skill.SkillType.backend)
+        {
+            this.skillNameBackgroundImage.sprite = this.backendSkillBackgroundImage;
+        }
+
+        this.skillNameText.SetText(_characterSkill.GetCharacterSubskillData().GetSubskillData().DisplayName);
+
+        int _speed = _characterSkill.GetCharacterSubskillData().GetSubskillData().Speed;
+        int _strength = _characterSkill.GetCharacterSubskillData().GetSubskillData().Strength;
+
+        if (_speed + _skillStatIncrement == 3)
+        {
+            this.speedEffectAnimator.Play("SpeedV2");
+        }
+        else if (_speed + _skillStatIncrement >= 4)
+        {
+            this.speedEffectAnimator.Play("GodSpeedV2");
+        }
+
+        if (_strength + _skillStatIncrement == 2)
+        {
+            this.strengthEffectAnimator.Play("Strength_1_V2");
+        }
+        else if (_strength + _skillStatIncrement >= 3)
+        {
+            this.strengthEffectAnimator.Play("Strength_2_V2");
+        }
+
+        this.skillNameGO.SetActive(true);
+        LeanTween.delayedCall(partADuration * preferredPercentage, Hide);
+    }
+
+    private void Hide()
+    {
+        this.skillNameGO.SetActive(false);
+        this.enemyPassiveSkillName.SetActive(false);
+        this.playerPassiveSkillName.SetActive(false);
+        this.speedEffectGO.gameObject.SetActive(false);
+        this.strengthEffectGO.gameObject.SetActive(false);
+    }
+}
