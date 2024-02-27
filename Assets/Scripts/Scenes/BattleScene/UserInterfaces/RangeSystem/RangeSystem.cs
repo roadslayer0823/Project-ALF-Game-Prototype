532701f62a59b3b50c6815b701eec5a61de82e10
LeeKahHin <kahhin@starsplash.com.my>
2024-02-27 12:33:52 +0800

    added setCurrentRange and getCurrentRange

--- Assets/Scripts/Scenes/BattleScene/UserInterfaces/RangeSystem/RangeSystem.cs
+++ Assets/Scripts/Scenes/BattleScene/UserInterfaces/RangeSystem/RangeSystem.cs
@@ -11,18 +11,27 @@ public class RangeSystem : MonoBehaviour
     [SerializeField] private RectTransform middlePosition2;
     [SerializeField] private RectTransform longPosition1;
     [SerializeField] private RectTransform longPosition2;
+    [SerializeField] private Range currentRange = Range.none; 
 
-    public void UpdateCurrentRangePosition(int currentRange)
+    public enum Range
     {
-        if(currentRange == 1)
+        none,
+        meleeRange,
+        middleRange,
+        longRange    
+    }
+
+    public void UpdateCurrentRangePosition(Range currentRange)
+    {
+        if(currentRange == Range.meleeRange)
         {
             SetToMeleePosition();
         }
-        else if(currentRange == 2)
+        else if(currentRange == Range.middleRange)
         {
             SetToMiddlePosition();
         }
-        else if(currentRange == 3)
+        else if(currentRange == Range.longRange)
         {
             SetToLongPosition();
         }
@@ -30,19 +39,32 @@ public class RangeSystem : MonoBehaviour
 
     public void SetToMeleePosition()
     {
+        SetCurrentRange(Range.meleeRange);
         pointer1.rectTransform.position = meleePosition1.position;
         pointer2.rectTransform.position = meleePosition2.position;
     }
 
     public void SetToMiddlePosition()
     {
+        SetCurrentRange(Range.middleRange);
         pointer1.rectTransform.position = middlePosition1.position;
         pointer2.rectTransform.position = middlePosition2.position;
     }
 
     public void SetToLongPosition()
     {
+        SetCurrentRange(Range.longRange);
         pointer1.rectTransform.position = longPosition1.position;
         pointer2.rectTransform.position = longPosition2.position;
+    }
+
+    public void SetCurrentRange(Range currentRangeStatus)
+    {
+        currentRange = currentRangeStatus;
+    }
+
+    public Range GetCurrentRange()
+    {
+        return currentRange;
     }
 }
