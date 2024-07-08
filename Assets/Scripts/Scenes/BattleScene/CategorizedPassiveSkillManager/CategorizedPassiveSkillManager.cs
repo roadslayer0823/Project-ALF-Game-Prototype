47570d1594eb2e22833e5e58a58b5e22b8dd2236
LeeKahHin <kahhin@starsplash.com.my>
2024-07-08 13:15:41 +0800

    no message

--- Assets/Scripts/Scenes/BattleScene/CategorizedPassiveSkillManager/CategorizedPassiveSkillManager.cs
+++ Assets/Scripts/Scenes/BattleScene/CategorizedPassiveSkillManager/CategorizedPassiveSkillManager.cs
@@ -44,11 +44,17 @@ public partial class CategorizedPassiveSkillManager : MonoBehaviour
     public const string PASSIVE_SKILL_ID_PSS11 = "PSS11";
     public const string PASSIVE_SKILL_ID_PSS12 = "PSS12";
 
-    // <此以太消耗為"最終以太消耗">
+    //<此以太消耗為"最終以太消耗">
     public static float finalTotalStatePointCost = 0;
 
     //<此以太消耗為"角力以太消耗">
     public static float jiaoLiStatePointCost = 0;
+
+    //<此為"回避壓力消耗>
+    public static float stressEvasionCost = 0;
+
+    //<此為"回避壓力消耗以太提升")
+    public static float stressEvasionMaxStatePointIncrease = 0;
 
     public enum CategoryType
     {
