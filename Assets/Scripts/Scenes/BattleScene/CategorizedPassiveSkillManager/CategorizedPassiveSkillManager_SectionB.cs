f338175e41b6487cb437a5e86ed1a898cdce1ae9
Yeong Chu Yao <chuyao@starsplash.com.my>
2024-07-16 13:46:34 +0800

    - Remove unnecessary using classes

--- Assets/Scripts/Scenes/BattleScene/CategorizedPassiveSkillManager/CategorizedPassiveSkillManager_SectionB.cs
+++ Assets/Scripts/Scenes/BattleScene/CategorizedPassiveSkillManager/CategorizedPassiveSkillManager_SectionB.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Rendering.FilterWindow;
 using PassiveSkill = DatabaseManager.PassiveSkill;
 
 public partial class CategorizedPassiveSkillManager : MonoBehaviour
